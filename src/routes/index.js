978ce71ba9a4c5a02cdd3b5db161b0a381bd7325
ShinThantNaung <shinthantnaung112@gmail.com>
2026-04-12 23:33:00 +0630

    feat(routes): Create general routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const authRoutes = require('./auth/routes');
+const authRoutes = require('./module/auth/routes');
 const socketRoutes = require('./socket/routes');
+const pingRoutes = require('./module/ping/routes')
 
 router.use('/auth', authRoutes);
+router.use('/ping',pingRoutes);
 router.use('/socket', socketRoutes);
 
 module.exports = router;
